3a9759b034a9e79ac7ae8452c2a9bf67c96231d4
Evgeny Zhlobo <evgen.zhlobo@gmail.com>
2014-05-03 03:06:35 +0300

    Update tests

--- test/unit/selectors.js
+++ test/unit/selectors.js
@@ -2,117 +2,175 @@ new function () {
 
 	module( "Selectors" );
 
-	test( "Standart", function() {
-		deepEqual(
-			ha(), ja(),
-			"hata() return empty hata object");
+	test( 'Standart', function() {
 
 		deepEqual(
-			ha( "" ), ja( "" ),
-			"hata( '' ) return empty hata object");
+			new Hata().items, [],
+			'new Hata() returns empty hata object');
 
 		deepEqual(
-			ha( false ), ja( false ),
-			"hata( false ) return empty hata object");
-
-		// Not like jQuery
-		deepEqual(
-			ha( true ), [],
-			"hata( true ) return empty hata object");
+			new Hata('body').items, [document.body],
+			'new Hata( "body" )');
 
 		deepEqual(
-			ha( null ), ja( null ),
-			"hata( null ) return empty hata object");
+			new Hata('#' + ID).items, ja('#' + ID),
+			'new Hata( "#'+ID+'" )');
 
 		deepEqual(
-			ha( undefined ), ja( undefined ),
-			"hata( undefined ) return empty hata object");
+			new Hata('#' + ID + ', #' + ID + '-h1').items, ja('#' + ID + ', #' + ID + '-h1'),
+			'new Hata( "#'+ID+', #'+ID+'-h1" )');
 
 		deepEqual(
-			ha( window ), ja( window ),
-			"hata( window )");
+			new Hata('.foo').items, ja('.foo'),
+			'new Hata( ".foo" )');
 
 		deepEqual(
-			ha( document ), ja( document ),
-			"hata( document )");
+			new Hata('.foo, .bar').items, ja('.foo, .bar'),
+			'new Hata( ".foo, .bar" )');
+
+		ok(
+			new Hata('.foo, .foo').items.length == 1,
+			'new Hata( ".foo, .foo" ) returns hata object without doubles');
 
 		deepEqual(
-			ha( "body" ), ja( "body" ),
-			"hata( 'body' )");
+			new Hata('.foo.foo-class').items, ja('.foo.foo-class'),
+			'new Hata( ".foo.foo-class" )');
 
 		deepEqual(
-			ha( "#" + ID ), ja( "#" + ID ),
-			"hata( '#ID' )");
+			new Hata('*').items, ja('*'),
+			'new Hata( "*" )');
 
 		deepEqual(
-			ha( "#" + ID + ", #" + ID + "-h1" ), ja( "#" + ID + ", #" + ID + "-h1" ),
-			"hata( '#ID, #ID2' )");
+			new Hata('[title*="es"]').items, ja('[title*="es"]'),
+			'new Hata( "[title*=\"value\"]" )');
 
 		deepEqual(
-			ha( ".foo" ), ja( ".foo" ),
-			"hata( '.class' )");
+			new Hata('a').items, ja('a'),
+			'new Hata( "a" )');
 
 		deepEqual(
-			ha( ".foo, .bar" ), ja( ".foo, .bar" ),
-			"hata( '.class, .class' )");
+			new Hata('notexistedselector').items, ja('notexistedselector'),
+			'new Hata( "notexistedselector" )');
 
-		deepEqual(
-			ha( ".foo.foo-class" ), ja( ".foo.foo-class" ),
-			"hata( '.class.class' )");
-
-		deepEqual(
-			ha( "*" ), ja( "*" ),
-			"hata( '*' )");
-
-		deepEqual(
-			ha( "[title*=\"es\"]" ), ja( "[title*=\"es\"]" ),
-			"hata( '[attr*=\"value\"]' )");
-
-		deepEqual(
-			ha( "a" ), ja( "a" ),
-			"hata( 'tag' )");
-
-		deepEqual(
-			ha( "asd" ), ja( "asd" ),
-			"hata( 'unknown' ) return empty hata object");
-
-		deepEqual(
-			ha( document.querySelectorAll( "p" ) ), ja( document.querySelectorAll( "p" ) ),
-			"hata( document.querySelectorAll( 'tag' ) )");
-
-		deepEqual(
-			ha( document.getElementsByTagName( "p" ) ), ja( document.getElementsByTagName( "p" ) ),
-			"hata( document.getElementsByTagName( 'p' ) )");
-
-		deepEqual(
-			ha( document.getElementById( ID ) ), ja( document.getElementById( ID ) ),
-			"hata( document.getElementById( 'ID' ) )");
-
-		deepEqual(
-			ha( hata( "a" ) ), ja( "a" ),
-			"hata( hata( 'tag' ) )");
 	});
 
-	test( "With context", function() {
-		deepEqual(
-			ha( "#" + ID + "-h1", "#" + ID ), ja( "#" + ID + "-h1", "#" + ID ),
-			"hata( '#ID2', '#ID1' ) [Node #ID2 inside #ID1]");
+	test( 'Friendly selectors', function() {
 
 		deepEqual(
-			ha( "#" + ID, "#" + ID + "-h1" ), ja( "#" + ID, "#" + ID + "-h1" ),
-			"hata( '#ID1', '#ID2' ) [Node #ID2 inside #ID1] return empty hata object");
+			new Hata('').items, [],
+			'new Hata( "" ) returns empty hata object');
 
 		deepEqual(
-			ha( "#" + ID + "-h1", hata( "#" + ID ) ), ja( "#" + ID + "-h1", "#" + ID ),
-			"hata( '#ID2', hata( '#ID1' ) ) [Node #ID2 inside #ID1]");
+			new Hata(false).items, [],
+			'new Hata( false ) returns empty hata object');
 
 		deepEqual(
-			ha( "#" + ID, hata( "#" + ID + "-h1" ) ), ja( "#" + ID, "#" + ID + "-h1" ),
-			"hata( '#ID1', hata( '#ID2' ) ) [Node #ID2 inside #ID1] return empty hata object");
+			new Hata(true).items, [],
+			'new Hata( true ) returns empty hata object');
 
 		deepEqual(
-			ha( "a", "p" ), ja( "a", "p" ),
-			"hata( 'tag', 'tag' )");
+			new Hata(null).items, [],
+			'new Hata( null ) returns empty hata object');
+
+		deepEqual(
+			new Hata(undefined).items, [],
+			'new Hata( undefined ) returns empty hata object');
+
+		// deepEqual(
+		// 	new Hata(window).items, [window],
+		// 	'new Hata( window )');
+
 	});
+
+	// test( "Standart", function() {
+	// 	deepEqual(
+	// 		ha(), ja(),
+	// 		"hata() return empty hata object");
+
+	// 	deepEqual(
+	// 		ha( "" ), ja( "" ),
+	// 		"hata( '' ) return empty hata object");
+
+	// 	deepEqual(
+	// 		ha( false ), ja( false ),
+	// 		"hata( false ) return empty hata object");
+
+	// 	// Not like jQuery
+	// 	deepEqual(
+	// 		ha( true ), [],
+	// 		"hata( true ) return empty hata object");
+
+	// 	deepEqual(
+	// 		ha( null ), ja( null ),
+	// 		"hata( null ) return empty hata object");
+
+	// 	deepEqual(
+	// 		ha( undefined ), ja( undefined ),
+	// 		"hata( undefined ) return empty hata object");
+
+	// 	deepEqual(
+	// 		ha( window ), ja( window ),
+	// 		"hata( window )");
+
+	// 	deepEqual(
+	// 		ha( document ), ja( document ),
+	// 		"hata( document )");
+
+	// 	deepEqual(
+	// 		ha( "body" ), ja( "body" ),
+	// 		"hata( 'body' )");
+
+	// 	deepEqual(
+	// 		ha( "#" + ID ), ja( "#" + ID ),
+	// 		"hata( '#ID' )");
+
+	// 	deepEqual(
+	// 		ha( "#" + ID + ", #" + ID + "-h1" ), ja( "#" + ID + ", #" + ID + "-h1" ),
+	// 		"hata( '#ID, #ID2' )");
+
+	// 	deepEqual(
+	// 		ha( ".foo" ), ja( ".foo" ),
+	// 		"hata( '.class' )");
+
+	// 	deepEqual(
+	// 		ha( ".foo, .bar" ), ja( ".foo, .bar" ),
+	// 		"hata( '.class, .class' )");
+
+	// 	deepEqual(
+	// 		ha( ".foo.foo-class" ), ja( ".foo.foo-class" ),
+	// 		"hata( '.class.class' )");
+
+	// 	deepEqual(
+	// 		ha( "*" ), ja( "*" ),
+	// 		"hata( '*' )");
+
+	// 	deepEqual(
+	// 		ha( "[title*=\"es\"]" ), ja( "[title*=\"es\"]" ),
+	// 		"hata( '[attr*=\"value\"]' )");
+
+	// 	deepEqual(
+	// 		ha( "a" ), ja( "a" ),
+	// 		"hata( 'tag' )");
+
+	// 	deepEqual(
+	// 		ha( "asd" ), ja( "asd" ),
+	// 		"hata( 'unknown' ) return empty hata object");
+
+	// 	deepEqual(
+	// 		ha( document.querySelectorAll( "p" ) ), ja( document.querySelectorAll( "p" ) ),
+	// 		"hata( document.querySelectorAll( 'tag' ) )");
+
+	// 	deepEqual(
+	// 		ha( document.getElementsByTagName( "p" ) ), ja( document.getElementsByTagName( "p" ) ),
+	// 		"hata( document.getElementsByTagName( 'p' ) )");
+
+	// 	deepEqual(
+	// 		ha( document.getElementById( ID ) ), ja( document.getElementById( ID ) ),
+	// 		"hata( document.getElementById( 'ID' ) )");
+
+	// 	deepEqual(
+	// 		ha( hata( "a" ) ), ja( "a" ),
+	// 		"hata( hata( 'tag' ) )");
+	// });
 
 };
